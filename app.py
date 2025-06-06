54460d93a847d4754f3d1722c61c52b74e9db112
Diwan Singh Chauhan <diwansinghchauhan91@gmail.com>
2025-06-06 12:07:16 +0530

    update fuction predict

--- app.py
+++ app.py
@@ -13,27 +13,23 @@ with open('tokenizer_gru.pickle','rb') as handle:
     tokenizer=pickle.load(handle)
 
 
-def generate_text(model, tokenizer, seed_text, max_len, num_words):
-    output_text = seed_text
-    for _ in range(num_words):
-        token_list = tokenizer.texts_to_sequences([output_text])[0]
-        token_list = token_list[-(max_len - 1):]  # keep only the last max_len-1 tokens
-        token_list = pad_sequences([token_list], maxlen=max_len - 1, padding='pre')
-        
-        predicted = model.predict(token_list, verbose=0)
-        predicted_word_index = np.argmax(predicted, axis=1)[0]
-        
-        # Find the word by index
-        for word, index in tokenizer.word_index.items():
-            if index == predicted_word_index:
-                output_text += " " + word
-                break
-    return output_text
+# Function to predict the next word
+def predict_next_word(model, tokenizer, train_text, max_len):
+    token_list = tokenizer.texts_to_sequences([train_text])[0]
+    if len(token_list) >= max_len:
+        token_list = token_list[-(max_len-1):]  # Ensure the sequence length matches max_len-1
+    token_list = pad_sequences([token_list], maxlen=max_len-1, padding='pre')
+    predicted = model.predict(token_list, verbose=0)
+    predicted_word_index = np.argmax(predicted, axis=1)
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return None
 
 # streamlit app
 st.title("Next Word Prediction With LSTM")
 input_text=st.text_input("Enter the sequence of words")
 if st.button("Predict Next Word"):
         max_len=model.input_shape[1]+1
-        next_word = generate_text(model, tokenizer, input_text, max_len, num_words=3)
+        next_word=predict_next_word(model,tokenizer,input_text,max_len)
         st.write(f'Next word: {next_word}')
